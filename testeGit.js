1cb9f174ee518dbbde745024c1a32dc40cc4b59e
Francisco Mauro de Lima <francisco.mauro@fgv.br>
2019-02-20 10:51:58 -0300

    C2 in B1

--- testeGit.js
+++ testeGit.js
@@ -4,4 +4,13 @@ var Pessoa = (function(){
 	function Task(){
 	}
 	
+	Task.prototype.getName = function () {
+         return this.Name;
+     };
+
+	Task.prototype.setName = function (name) {
+         this.Name = name;
+     };
+
+	 
 })()
